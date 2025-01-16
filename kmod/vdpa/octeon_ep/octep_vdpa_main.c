nagendraputhane/dpu-accelerator-offload

commit 3fc40523215bbc5fba408ad22f6847a1af22af5c

kmod/vdpa: correct VF mailbox data address calculation
This patch corrects the PF->VF mailbox address calculation, fixing the
VF initialization issue when rings per VF exceed 1.

Signed-off-by: Srujana Challa <schalla@marvell.com>
Change-Id: Iba4cdbc3cbc46303f6d102efdda7cf45fa3dc89b
Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpu-offload/+/142001
Reviewed-by: Vamsi Krishna Attunuru <vattunuru@marvell.com>
Reviewed-by: Nithin Kumar Dabilpuram <ndabilpuram@marvell.com>
Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>

kmod/vdpa/octeon_ep/octep_vdpa_main.c
@@ -752,6 +752,8 @@ static int octep_sriov_enable(struct pci_dev *pdev, int num_vfs)
 	bool done = false;
 	int index = 0;
 	int ret, i;
+	u8 rpvf;
+	u64 val;
 
 	ret = pci_enable_sriov(pdev, num_vfs);
 	if (ret)
@@ -770,9 +772,11 @@ static int octep_sriov_enable(struct pci_dev *pdev, int num_vfs)
 		}
 	}
 
+	val = readq(addr + OCTEP_EPF_RINFO(0));
+	rpvf = FIELD_GET(GENMASK_ULL(35, 32), val);
 	if (done) {
 		for (i = 0; i < pf->enabled_vfs; i++)
-			writeq(OCTEP_DEV_READY_SIGNATURE, addr + OCTEP_PF_MBOX_DATA(i));
+			writeq(OCTEP_DEV_READY_SIGNATURE, addr + OCTEP_PF_MBOX_DATA(i * rpvf));
 	}
 
 	return num_vfs;

commit ffe5b1bfaa9d12fb12b575b53c6cd824741dfbb1

kmod/vdpa: include missing header
Added missing header (linux/bitfield.h) to fix build on v6.8 kernel.

 error: implicit declaration of function _FIELD_GET_
 [-Werror=implicit-function-declaration]

Fixes: 3fc40523215b ("kmod/vdpa: correct VF mailbox data address
		     calculation")
Cc: schalla@marvell.com

Signed-off-by: Shijith Thotton <sthotton@marvell.com>
Change-Id: Ia62693db4d630f41c79a4e3558fef351deed4c19
Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpu-offload/+/142454
Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
Reviewed-by: Srujana Challa <schalla@marvell.com>

kmod/vdpa/octeon_ep/octep_vdpa_main.c
@@ -2,6 +2,7 @@
  * Copyright (c) 2023 Marvell.
  */
 
+#include <linux/bitfield.h>
 #include <linux/interrupt.h>
 #include <linux/io-64-nonatomic-lo-hi.h>
 #include <linux/module.h>